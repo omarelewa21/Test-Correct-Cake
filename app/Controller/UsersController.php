41c7bbe4a42057ae370a460687721bdd42a92681
Erik | Sobit <Erik@sobit.nl>
2020-08-20 10:53:09 +0200

    fix issue with onboarding wizard report => renaming to marketing report

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -293,7 +293,7 @@ class UsersController extends AppController
             if(!$result) exit;
 
             $this->response->body($result);
-            $this->response->header('Content-Disposition', 'attachment; filename=onboarding_wizard_report_'.date('YMDHi').'.xls');
+            $this->response->header('Content-Disposition', 'attachment; filename=marketing_report_'.date('YmdHi').'.xls');
             return $this->response;
     }
 
