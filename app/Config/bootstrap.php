efd924eac86f76223c5b8fd166d746e5150aea43
Jonathan <jonathanjagt@teachandlearncompany.com>
2020-08-21 15:07:34 +0200

    Add global UUID function
    getUUID is used to retrieve the UUID from an array or get the ID if not found

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1,5 +1,65 @@
 <?php
 require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+if (!function_exists('getUUID')) {
+
+	function getUUID($item, $case, $options = []) {
+
+		if(is_array($item) && !key_exists('uuid', $item)) {
+
+			//set uuid to id if not exists
+			//because in the switch statement 
+			//we can assume that the ['uuid'] 
+			//does always exists
+			if (key_exists('id', $item)) {
+				$item['uuid'] = $item['id'];
+			}
+		}
+
+		switch ($case) {
+			case 'get':
+				
+				if (is_array($item)) {
+					return $item['uuid'];
+				}
+				
+				return $item;
+
+				break;
+
+			case 'getQuoted':
+
+				if (is_array($item)) {
+					return "'" . $item['uuid'] . "'";
+				}
+				
+				return "'" . $item . "'";
+
+				break;
+
+			case 'format':
+				
+				if (is_string($item['uuid'])) {
+					$format = "%s";
+				} elseif (is_int($item['uuid'])) {
+					$format = "%d";
+				}
+
+				$url = str_replace('%%', $format, $options['formatString']);
+
+				return sprintf($url, $item['uuid']);
+
+				break;
+			
+			default:
+				return null;
+				break;
+		}
+
+
+	}
+
+}
 /**
  * This file is loaded automatically by the app/webroot/index.php file after core.php
  *
