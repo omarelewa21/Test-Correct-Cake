2d23400486a2e9317da6a75f72507d1c16e4d215
Erik | Sobit <Erik@sobit.nl>
2020-10-23 16:43:57 +0200

    check on uuid instead of id

--- app/Controller/FileManagementController.php
+++ app/Controller/FileManagementController.php
@@ -119,7 +119,7 @@ class FileManagementController extends AppController
         } else {
             $educationLevel = '';
             foreach ($schoolLocationEducationLevels as $el) {
-                if ($el['education_level']['id'] == $data['typedetails']['education_level_id']) {
+                if (getUUID($el['education_level'],'get') == $data['typedetails']['education_level_id']) {
                     $educationLevel = $el['education_level']['name'];
                 }
             }
