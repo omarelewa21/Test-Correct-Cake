df5c459351b840fbe8b702b741bc1fe13b8db7da
roansobit <roan@sobit.nl>
2021-01-08 17:16:28 +0100

    File upload on submit & size validation

--- app/Controller/FileManagementController.php
+++ app/Controller/FileManagementController.php
@@ -5,6 +5,7 @@ App::uses('FileManagementService', 'Lib/Services');
 App::uses('SchoolLocationsService', 'Lib/Services');
 App::uses('UsersService', 'Lib/Services');
 App::uses('TestsService', 'Lib/Services');
+App::uses('HelperFunctions', 'Lib');
 
 class FileManagementController extends AppController {
 
@@ -346,6 +347,7 @@ class FileManagementController extends AppController {
             $this->set('testKindOptions', $testKinds);
         }
 
+        $this->set('max_file_upload_size',HelperFunctions::getInstance()->getMaxFileUploadSize());
         $this->set('form_id', md5(time()));
         $this->set('school_location_id', $school_location_id);
         $this->set('user_uuid', AuthComponent::user('uuid'));
--- app/Lib/HelperFunctions.php
+++ app/Lib/HelperFunctions.php
@@ -59,4 +59,24 @@ class HelperFunctions
         return html_entity_decode($var);
     }
 
+    public function getMaxFileUploadSize()
+    {
+        return $this->return_bytes(ini_get('upload_max_filesize'));
+    }
+
+    public function return_bytes($val) {
+        $val = trim($val);
+        $last = strtolower($val[strlen($val)-1]);
+        switch($last)
+        {
+            case 'g':
+                $val *= 1024;
+            case 'm':
+                $val *= 1024;
+            case 'k':
+                $val *= 1024;
+        }
+        return $val;
+    }
+
 }
