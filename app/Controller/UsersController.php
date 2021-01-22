24ff8fb953dd1dac88b0c7acde4568196be68cdd
Frits <frits.rincker@test-correct.nl>
2021-01-22 16:10:42 +0100

    Remove comment // tell a teacher

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1015,7 +1015,7 @@ class UsersController extends AppController
             $menus['knowledgebase'] = "Kennisbank";
 
             if ($role['name'] == 'Teacher') {
-                //$menus['tell_a_teacher'] = "<i class='fa fa-bullhorn' style='color:#FF3333;font-weight:bold;'></i> Nodig een collega uit!";
+                $menus['tell_a_teacher'] = "<i class='fa fa-bullhorn' style='color:#FF3333;font-weight:bold;'></i> Nodig een collega uit!";
             }
         }
 
