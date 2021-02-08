246cb798f70f1bdb4890f07ddb6b92cca27d6e95
Gert-Mark <info@gmwconsult.nl>
2021-02-08 09:10:17 +0000

    bug concerning closeWithNewPopup

--- app/webroot/js/test.js
+++ app/webroot/js/test.js
@@ -68,7 +68,7 @@ var Test = {
                 if ($('#groupquestion_type_confirm').hasClass("disabled")) {
                     return false;
                 }
-                var url = '/questions/add_group/'+this.test_id+'/'+this.groupQuestionType;
+                var url = '/questions/add_group/'+testObj.test_id+'/'+testObj.groupQuestionType;
                 Popup.closeWithNewPopup(url);
             });
 
