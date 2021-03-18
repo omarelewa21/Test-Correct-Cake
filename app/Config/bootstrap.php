a728a8fc4c7457a40b530495cde264259c86fe03
Elewa <omarelewa21@gmail.com>
2021-03-18 22:29:37 +0200

    Display English translation

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -193,7 +193,7 @@ Configure::write('Dispatcher.filters', array(
 	'AssetDispatcher',
 	'CacheDispatcher'
 ));
-
+Configure::write('Config.language', 'eng');
 /**
  * Configures default file logging options
  */
