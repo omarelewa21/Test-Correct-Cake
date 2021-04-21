2be9cf6f9163e706202f633108e376cb165df307
Elewa <omarelewa21@gmail.com>
2021-04-21 09:44:00 +0200

    translation modified

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -91,12 +91,13 @@ class AppController extends Controller
         $this->Auth->allow('get_header_session');
 
         if (!($this->Auth->loggedIn())) {
-        $language =substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+            $language =substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
-        if($language !=	 'nl'){
-            $this->Session->write('Config.language', 'eng');
-        } 
+            if($language !=	 'nl'){
+                $this->Session->write('Config.language', 'eng');
+            } 
         }
+        
     }
 
 
