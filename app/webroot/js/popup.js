811ccb59e315519af8a1e8a196b5357104978c43
Erik | Sobit <Erik@sobit.nl>
2021-05-18 15:15:40 +0200

    prevent double click on messageOk

--- app/webroot/js/popup.js
+++ app/webroot/js/popup.js
@@ -460,6 +460,11 @@ var Popup = {
     },
 
     messageOk: function () {
+        if(this.hasDebounceLock()){
+            return;
+        }
+        this.setDebounceLock();
+
         var index = Popup.index;
 
         if (Popup.callback != null) {
