a3c20063f50439858a8f862dca4192386bba34f3
J. Verzijden <johanverzijden@teachandlearncompany.com>
2021-06-15 17:40:59 +0200

    also added the pastetools plugin

--- app/webroot/ckeditor/config.js
+++ app/webroot/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.plugins.addExternal('ckeditor_wiris', 'plugins/@wiris/mathtype-ckeditor
 
 
 CKEDITOR.editorConfig = function( config ) {
-    config.extraPlugins = 'clipboard,pastefromgdocs,pastefromword,advanced,simpleuploads,quicktable,panelbutton,button,floatpanel,panel,ckeditor_wiris';
+    config.extraPlugins = 'clipboard,pastetools,pastefromgdocs,pastefromword,advanced,simpleuploads,quicktable,panelbutton,button,floatpanel,panel,ckeditor_wiris';
     config.allowedContent = true;
     config.disableNativeSpellChecker = true;
 
