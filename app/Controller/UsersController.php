17b407d65465686c1936d36e0c74cae9242057f6
roansobit <roan@sobit.nl>
2021-07-06 13:52:35 +0200

    Disabled discussion in menu, as it shouldnt be enabled

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1446,12 +1446,12 @@ class UsersController extends AppController
                     'path'  => '/test_takes/taken_teacher'
                 );
 
-                $tiles['tests_discussed'] = array(
-                    'menu' => 'taken',
-                    'icon' => 'bespreken',
-                    'title' => 'Bespreken',
-                    'path' => '/test_takes/discussion'
-                );
+//                $tiles['tests_discussed'] = array(
+//                    'menu' => 'taken',
+//                    'icon' => 'bespreken',
+//                    'title' => 'Bespreken',
+//                    'path' => '/test_takes/discussion'
+//                );
 
                 $tiles['tests_examine'] = array(
                     'menu'  => 'taken',
