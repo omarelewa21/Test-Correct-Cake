30e490fd353705e93b72c213bdcbdced69d72d4a
Martin Folkerts <m.folkerts@sobit.nl>
2021-08-19 11:01:25 +0200

    removed version 2.3

--- app/Lib/AppVersionDetector.php
+++ app/Lib/AppVersionDetector.php
@@ -88,7 +88,6 @@ class AppVersionDetector
                 "3.0.4",
                 "3.0.5",
                 "3.1.0",
-                "2.3",
             ],
             "needsUpdate" => [
             ],
