e17dd569803ae11a8c888f63849a4bbb362e2375
Elewa <omarelewa21@gmail.com>
2021-08-27 19:13:13 +0200

    translation modified v4

--- app/webroot/js/welcome-messages.js
+++ app/webroot/js/welcome-messages.js
@@ -8,7 +8,7 @@ var supportsES6 = function() {
         notice.innerHTML = `
         <div class="notification warning" style="background-color: #FFEDEB; padding:5px; padding-left:10px; border-color: #EBCCC8;">
             <div class="body">
-                <p style="color: red">` + $.i18n('U gebruikt een verouderde browser. Deze browser wordt niet meer ondersteund vanaf 2 april 2021. Kies een andere browser of doe een upgrade') + `.</p>
+                <p style="color: red">` + 'U gebruikt een verouderde browser. Deze browser wordt niet meer ondersteund vanaf 2 april 2021. Kies een andere browser of doe een upgrade' + `.</p>
             </div>
         </div>`;
 
