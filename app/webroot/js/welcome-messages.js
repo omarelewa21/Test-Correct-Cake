d0bdf8aeabb66f2c81fb646399cc1f5ee64ef5a2
Hashtechs <omarelewa21@gmail.com>
2021-09-30 15:33:41 +0200

    comment Es6 detection function as master branch

--- app/webroot/js/welcome-messages.js
+++ app/webroot/js/welcome-messages.js
@@ -1,25 +1,27 @@
-let supportsES6 = function() {
-    try {
-        new Function("(a = 0) => a");
-        return true;
-    }
-    catch (err) {
-        var notice = document.createElement("div");
-        notice.innerHTML = `
-        <div class="notification warning" style="background-color: #FFEDEB; padding:5px; padding-left:10px; border-color: #EBCCC8;">
-            <div class="body">
-                <p style="color: red">` + 'U gebruikt een verouderde browser. Deze browser wordt niet meer ondersteund vanaf 2 april 2021. Kies een andere browser of doe een upgrade' + `.</p>
-            </div>
-        </div>`;
-
-        var notes = document.getElementsByClassName("notes")[0];
-        if(typeof notes == 'undefined'){
-            var notes = document.getElementById("container");
-        }
-        notes.insertBefore(notice, notes.firstChild);
-        return false;                   
-    }
+var supportsES6 = function() {
+    return true;
+    // try {
+    //     new Function("(a = 0) => a");
+    //     return true;
+    // }
+    // catch (err) {
+    //     var notice = document.createElement("div");
+    //     notice.innerHTML = `
+    //     <div class="notification warning" style="background-color: #FFEDEB; border-color: #EBCCC8;">
+    //         <div class="body">
+    //             <p style="color: red">U gebruikt een verouderde browser. Deze browser wordt niet meer ondersteund vanaf 2 april 2021. Kies een andere browser of doe een upgrade.</p>
+    //         </div>
+    //     </div>`;
+    //
+    //     var notes = document.getElementsByClassName("notes")[0];
+    //     if(typeof notes == 'undefined'){
+    //         var notes = document.getElementById("container");
+    //     }
+    //     notes.prepend(notice);
+    //     return false;
+    // }
 }();
+
 
 let setUserMenuButtonText = function(){
     $('#btnLogout').text($.i18n('Uitloggen'));
