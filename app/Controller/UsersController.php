1f04df3c9b19c3ee5eb84ffb0eaf60e8fefa79c3
Erik | Sobit <Erik@sobit.nl>
2021-12-16 14:17:54 +0100

    remove testing

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2356,7 +2356,6 @@ class UsersController extends AppController
                 $internalPage = substr($internalPage, 0, 1) === '/' ? $internalPage : '/'.$internalPage;
                 $this->set('internal_page',$internalPage);
                 $this->render('internal_redirect');
-                echo 'stop';exit;
                 return;
             }
         }
