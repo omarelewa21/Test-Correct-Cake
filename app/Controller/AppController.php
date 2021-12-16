99aba4ae73f19e0c578161f9c9c931c6cec9b124
Erik | Sobit <Erik@sobit.nl>
2021-12-16 15:22:44 +0100

    added extra TLCVersioncheckResult as naming/ casing in Laravel is different

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -292,6 +292,7 @@ class AppController extends Controller
             'TLCOs'                 => CakeSession::read('TLCOs'),
             'TLCHeader'             => CakeSession::read('TLCHeader'),
             'TLCVersionCheckResult' => CakeSession::read('TLCVersionCheckResult'),
+            'TLCVersioncheckResult' => CakeSession::read('TLCVersionCheckResult'),
         ];
     }
 
