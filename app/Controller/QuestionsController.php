a2ad42dc0cb26ab66ca09745cde4f5b72b97547d
Martin Folkerts <martinfolkerts@iMac-van-Martin.local>
2022-04-13 14:06:39 +0200

    fixed problems with preview

--- app/Controller/QuestionsController.php
+++ app/Controller/QuestionsController.php
@@ -280,7 +280,9 @@ class QuestionsController extends AppController
 
             case 'DrawingQuestion':
                 if ($question['zoom_group']) {
-                    $question['answer'] = $this->QuestionsService->getBase64EncodedCorrectionModelForDrawingQuestion($question['uuid']);
+                    $this->set('image',$this->QuestionsService->getBase64EncodedCorrectionModelForDrawingQuestion($question['uuid']));
+                } else {
+                    $this->set('image', $question['answer']);
                 }
                 $view = 'preview_drawing_answer';
                 break;
@@ -1781,26 +1783,5 @@ class QuestionsController extends AppController
         return $filterParams;
     }
 
-    private function getDrawingQuestionAnswerImage($question)
-    {
-        if (isset($question['answer']) && !empty($question['answer']) && empty($question['zoom_group'])) {
-            return $question['answer'];
-        }
 
-        $zoomGroup = json_decode($question['zoom_group'], true);
-        $svg = sprintf('<svg viewBox="%s %s %s %s" class="w-full h-full" id="" xmlns="http://www.w3.org/2000/svg">
-                    <g class="question-svg">%s</g>
-                    <g class="answer-svg">%s</g>
-                    <g id="grid-preview-svg" stroke="var(--all-BlueGrey)" stroke-width="1"></g>
-                </svg>',
-            $zoomGroup['x'],
-            $zoomGroup['y'],
-            $zoomGroup['width'],
-            $zoomGroup['height'],
-            base64_decode($question['question_svg']),
-            base64_decode($question['answer_svg'])
-        );
-
-        return $svg;
-    }
 }
