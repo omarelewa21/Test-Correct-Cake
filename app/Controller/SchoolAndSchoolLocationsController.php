17d975149abd3f2e80e674c8ee9d167079c6bc16
Erik | Sobit <Erik@sobit.nl>
2022-05-17 16:10:01 +0200

    make it work from the cake side

---
+++ app/Controller/SchoolAndSchoolLocationsController.php
@@ -0,0 +1,63 @@
+<?php
+
+App::uses('AppController', 'Controller');
+App::uses('SchoolAndSchoolLocationsService', 'Lib/Services');
+App::uses('HelperFunctions','Lib');
+
+/**
+ * Sections controller
+ *
+ */
+class SchoolAndSchoolLocationsController extends AppController
+{
+    public function beforeFilter()
+    {
+        $this->DefaultSubjectsAndSectionsService = new SchoolAndSchoolLocationsService();
+        parent::beforeFilter();
+    }
+
+    public function index()
+    {
+        $this->isAuthorizedAs(['Administrator']);
+    }
+
+    public function import() {
+        $this->isAuthorizedAs(['Administrator']);
+
+        $data = $this->request->data['SchoolsAndSchoolLocations'];
+
+        if(!$data['file']['tmp_name']){
+            $response = __("File niet gevonden om te importeren, probeer het nogmaals");
+        }else{
+            $r = $this->SchoolAndSchoolLocationsService->uploadData($data);
+
+            if(array_key_exists('errors',$r)){
+                $errors = [];
+                foreach($r['errors'] as $item){
+                    if(is_array($item)){
+                        foreach($item as $e){
+                            $errors[] = $e;
+                        }
+                    } else {
+                        $errors[] = $item;
+                    }
+                }
+                $response = implode('<br>',$errors);
+            }
+            else if(array_key_exists('error',$r)){
+                $response = $r['error'];
+            }
+            else{
+                $response = $r['data'];
+            }
+        }
+        echo "
+            <div id='response'>".$response."</div>
+            <script>
+                window.parent.handleSchoolAndSchoolLocationsImportResponse(document.getElementById('response').outerHTML);
+            </script>
+        ";
+        exit;
+    }
+
+}
