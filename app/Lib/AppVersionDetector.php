1b19a20fea0bf34b909f01284adb9a93d1a046c6
Erik | Sobit <Erik@sobit.nl>
2022-06-21 16:58:53 +0200

    added needs update for 2.4.1

--- app/Lib/AppVersionDetector.php
+++ app/Lib/AppVersionDetector.php
@@ -42,7 +42,6 @@ class AppVersionDetector
         ],
         "iOS" => [
             "ok" => [
-                "2.4.1",
                 "2.4.2",
                 "2.4.3",
                 "2.4.4",
@@ -86,9 +85,10 @@ class AppVersionDetector
                 "3.1.0",
             ],
             "needsUpdate" => [
+                "2.4.1",
             ],
             "needsUpdateDeadline" => [
-
+                "2.4.1" => "24 juli 2022",
             ],
         ],
         "ChromeOS" => [
