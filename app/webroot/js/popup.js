7f2428fd6009a620d85e264418bee3813fa162d5
G.M. ter Wee <gm@gms-mac-mini.home>
2022-06-29 10:12:41 +0200

    improved popup ajax callbacks

--- app/webroot/js/popup.js
+++ app/webroot/js/popup.js
@@ -569,50 +569,41 @@ var Popup = {
     },
 
     showPreviewTest: function (testId) {
-        var url;
-        var liveUrl = window.location.protocol+'//'+window.location.host.replace('portal.test-','welcome.test-');
+        var windowReference = window.open();
         $.ajax({
             type: 'post',
             url: '/tests/get_preview_url/' + testId,
             dataType: 'json',
             data: {},
             success: function (data) {
-                var windowReference = window.open(liveUrl);
-                url = Core.getCorrectLaravelUrl(data.data.url);
-                windowReference.location = url;
+                windowReference.location = Core.getCorrectLaravelUrl(data.data.url);
                 windowReference.focus();
             }
         });
     },
 
     showPreviewAnswerModelTest: function (testId) {
-        var url;
-        var liveUrl = window.location.protocol+'//'+window.location.host.replace('portal.test-','welcome.test-').replace('portal2.test-','welcome2.test-');
+        var windowReference = window.open();
         $.ajax({
             type: 'post',
             url: '/tests/get_preview_answer_model_url/' + testId,
             dataType: 'json',
             data: {},
             success: function (data) {
-                var windowReference = window.open(liveUrl);
-                url = Core.getCorrectLaravelUrl(data.data.url);
-                windowReference.location = url;
+                windowReference.location = Core.getCorrectLaravelUrl(data.data.url);
                 windowReference.focus();
             }
         });
     },
     showPreviewTestTakeAnswers: function (testTakeId) {
-        var url;
-        var liveUrl = window.location.protocol+'//'+window.location.host.replace('portal.test-','welcome.test-').replace('portal2.test-','welcome2.test-');
+        var windowReference = window.open();
         $.ajax({
             type: 'post',
             url: '/test_takes/get_preview_test_take_answers_url/' + testTakeId,
             dataType: 'json',
             data: {},
             success: function (data) {
-                var windowReference = window.open(liveUrl);
-                url = Core.getCorrectLaravelUrl(data.data.url);
-                windowReference.location = url;
+                windowReference.location = Core.getCorrectLaravelUrl(data.data.url);
                 windowReference.focus();
             }
         });
