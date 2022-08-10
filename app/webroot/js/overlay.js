1e9188829a4e4c430fba781b517c72a7b3949629
G.M. ter Wee <gm@GMs-Mac-mini.local>
2022-08-10 13:38:00 +0200

    moved ckeditor init logic to ckeditor_tlc_methods.js, build in delay for initialising wsc after editor ready

--- app/webroot/js/overlay.js
+++ app/webroot/js/overlay.js
@@ -1,13 +1,25 @@
 var Overlay = {
     zIndex: 120,
 
-    overCkeditor4: function (selector,editor) {
+    overCkeditor4: function (selector,editor,repeat = false) {
         var overlay = document.querySelector(selector);
+        if(!overlay&&!repeat){
+            return setTimeout(Overlay.overCkeditor4(selector,editor,true),1000);
+        }
+        if(!overlay){
+            return;
+        }
         // overlay.style.border =  '1px solid red';
         overlay.style.zIndex = this.zIndex;
         overlay.style.position = 'absolute';
         var containerId = editor.container.$.id;
         setTimeout(function(){
+            if(!document.querySelector('#'+containerId)&&!repeat){
+                return setTimeout(Overlay.overCkeditor4(selector,editor,true),1000);
+            }
+            if(!document.querySelector('#'+containerId)){
+                return ;
+            }
             overlay.style.width = document.querySelector('#'+containerId).clientWidth+'px';
             overlay.style.height = document.querySelector('#'+containerId).clientHeight+'px';
         },1000);
