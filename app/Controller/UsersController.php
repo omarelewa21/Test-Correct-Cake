4e9bb2cf1922ec59982ba5585cc78563d18afab8
Erik | Sobit <Erik@sobit.nl>
2022-11-02 13:44:42 +0100

    added new link for knowledgebase

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1357,7 +1357,7 @@ class UsersController extends AppController
             'menu'  => 'support',
             'icon'  => 'knowledgebase',
             'title' => __("Kennisbank"),
-            'path'  => 'https://support.test-correct.nl',
+            'path'  => 'https://support.test-correct.nl/knowledge',
             'type'  => 'externalpopup',
         ];
 
