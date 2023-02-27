5d787bc92b400f10aab70c9986836343db5993e8
Omar Elewa <omar.elewa@test-correct.nl>
2023-02-27 04:29:38 +0200

    check school_location license_type before displaying the terms and conditions

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2440,7 +2440,12 @@ class UsersController extends AppController
 
     private function handleGeneralTermsForUser($userGeneralTermsLog = null)
     {
-        $shouldDisplayGeneralTermsNotification = $userGeneralTermsLog != null && $userGeneralTermsLog['accepted_at'] == null;
+        if(AuthComponent::user('school_location')['license_type'] === 'CLIENT'){
+            $shouldDisplayGeneralTermsNotification = false;
+        }else{
+            $shouldDisplayGeneralTermsNotification = $userGeneralTermsLog != null && $userGeneralTermsLog['accepted_at'] == null;
+        }
+
         $this->set('shouldDisplayGeneralTermsNotification', $shouldDisplayGeneralTermsNotification);
         if ($shouldDisplayGeneralTermsNotification) {
             $firstRequest = new DateTime($userGeneralTermsLog['created_at']);
