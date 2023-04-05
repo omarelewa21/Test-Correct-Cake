0d3455b9f55ae160ef87ebea11ecec4d235a79c0
Erik | Sobit <Erik@sobit.nl>
2023-04-05 10:46:21 +0200

    remove console log

--- app/webroot/js/test_take.js
+++ app/webroot/js/test_take.js
@@ -644,7 +644,6 @@ var TestTake = {
     },
 
     checkStartDiscussion: function (take_id, consists_only_closed_question = false, hasNonActiveParticipant=false, allow_new_teacher_co_learning = false) {
-        console.log('has non active participant '+hasNonActiveParticipant);
         if ( hasNonActiveParticipant || $('.participant:not(".active")').length > 0) {
             Popup.message({
                 btnOk: $.i18n('Ja'),
